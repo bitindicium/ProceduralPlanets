8a90712fb5cfa4bd63131a226af3d7452dd834ed
mboyle <m@mlboyle.com>
2018-09-21 20:57:41 -0400

    Episode 1.

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -2,15 +2,52 @@
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [Range(2, 256)]
+   public int resolution = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   [SerializeField, HideInInspector]
+   private MeshFilter[] meshFilters;
+
+   private TerrainFace[] terrainFaces;
+
+   private void OnValidate()
+   {
+      Initialize();
+      GenerateMesh();
+   }
+
+   void Initialize()
+   {
+      if (meshFilters == null || meshFilters.Length == 0)
+      {
+         meshFilters = new MeshFilter[6];
+      }
+
+      terrainFaces = new TerrainFace[6];
+
+      var directions = new [] { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+      for (int i = 0; i < 6; i++)
+      {
+         if (meshFilters[i] == null)
+         {
+            var meshObj = new GameObject("mesh");
+            meshObj.transform.parent = transform;
+
+            meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+            meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+            meshFilters[i].sharedMesh = new Mesh();
+         }
+
+         terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+      }
+   }
+
+   void GenerateMesh()
+   {
+      foreach (var face in terrainFaces)
+      {
+         face.ConstructMesh();
+      }
+   }
 }
--- Assets/TerrainFace.cs
+++ Assets/TerrainFace.cs
@@ -1,4 +1,55 @@
-﻿public class TerrainFace
-{
+﻿using UnityEngine;
 
+public class TerrainFace
+{
+   private Mesh mesh;
+   private int resolution;
+   private Vector3 localUp;
+   private Vector3 axisA, axisB;
+
+   public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+   {
+      this.mesh = mesh;
+      this.resolution = resolution;
+      this.localUp = localUp;
+      
+      axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+      axisB = Vector3.Cross(localUp, axisA);
+   }
+
+   public void ConstructMesh()
+   {
+      var vertices = new Vector3[resolution * resolution];
+      var triangles = new int[(resolution - 1) * (resolution - 1) * 2 * 3];
+      var triangleIndex = 0;
+
+      for (int y = 0; y < resolution; ++y)
+      {
+         for (int x = 0; x < resolution; ++x)
+         {
+            var i = x + y * resolution;
+            var percent = new Vector2(x, y) / (resolution - 1);
+            var pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
+            var pointOnUnitSpere = pointOnUnitCube.normalized;
+
+            vertices[i] = pointOnUnitSpere;
+
+            if (x != resolution - 1 && y != resolution - 1)
+            {
+               triangles[triangleIndex++] = i;
+               triangles[triangleIndex++] = i + resolution + 1;
+               triangles[triangleIndex++] = i + resolution;
+               
+               triangles[triangleIndex++] = i;
+               triangles[triangleIndex++] = i + 1;
+               triangles[triangleIndex++] = i + resolution + 1;
+            }
+         }
+      }
+
+      mesh.Clear();
+      mesh.vertices = vertices;
+      mesh.triangles = triangles;
+      mesh.RecalculateNormals();
+   }
 }
